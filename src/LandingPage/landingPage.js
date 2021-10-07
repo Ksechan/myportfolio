85daec25d75fb4524c0a247958a2007d196d53e8
Sechan Kim <rlatpcks77@gmail.com>
2021-10-07 18:04:16 +0900

    landingPage 애니메이션 효과 적용

--- src/LandingPage/landingPage.js
+++ src/LandingPage/landingPage.js
@@ -18,50 +18,60 @@ function LandingPage() {
         
         <div className="sechan-img">
         </div>
+  
 
+        <div className="landing-page-container">
 
-          <div className="landing-page-container">
-            <div className="text-animate"
+            <div className="first-grow-container">
+              <div className="first-grow"
               onMouseEnter={() => { setAboutMouseOver(true) }}
-                 onMouseLeave={() => { setAboutMouseOver(false) }}>
-              <Link to="/About" className={`hello ${aboutMouseOver ? '' : 'hide'}`}>
-                <div>
+              onMouseLeave={() => { setAboutMouseOver(false) }}>
+                <Link to="/About" className={`hello ${aboutMouseOver ? '' : 'hide'}`}>
+                  <div>
                   About
-                </div>
-              </Link>
-              <div className={`hello ${aboutMouseOver ? 'hide' : ''}`}>
+                  </div>
+                </Link>
+                <div className={`hello ${aboutMouseOver ? 'hide' : ''}`}>
                 Hello.
-              </div>
+                </div>
             </div>
+          </div>
+
 
         
-            <div className="text-animate"
-              onMouseEnter={() => { setWorkMouseOver(true) }}
-                onMouseLeave={() => { setWorkMouseOver(false) }}>
-              <Link to="/Work" className={`${workMouseOver ? '' : 'hide'}`}>
-                <div className="text-color-red ">
-                  Work
+            <div className="second-grow-container">
+              <div className="second-grow"
+                onMouseEnter={() => { setWorkMouseOver(true) }}
+                  onMouseLeave={() => { setWorkMouseOver(false) }}>
+                <Link to="/Work" className={`${workMouseOver ? '' : 'hide'}`}>
+                  <div className="text-color-red ">
+                    Work
+                  </div>
+                </Link>
+                <div className={`text-color-red ${workMouseOver ? 'hide' : ''}`}>
+                  I am
                 </div>
-              </Link>
-              <div className={`text-color-red ${workMouseOver ? 'hide' : ''}`}>
-                I am
               </div>
             </div>
 
+
+          <div className="third-grow-container">
+            <div className="third-grow"
+                onMouseEnter={() => { setContactMouseOver(true) }}
+                  onMouseLeave={() => { setContactMouseOver(false) }}>
+                <Link to="/Contact" className={`${contactMouseOver ? '' : 'hide'}`}>
+                  <div className="text-color-red ">
+                    Contact
+                  </div>
+                </Link>
+                <div className={`text-color-red ${contactMouseOver ? 'hide' : ''}`}>
+                  Sechan
+                </div>
+            </div>
+          </div>
+          
 
           
-            <div className="text-animate"
-              onMouseEnter={() => { setContactMouseOver(true) }}
-                onMouseLeave={() => { setContactMouseOver(false) }}>
-              <Link to="/Contact" className={`${contactMouseOver ? '' : 'hide'}`}>
-                <div className="text-color-red ">
-                  Contact
-                </div>
-              </Link>
-              <div className={`text-color-red ${contactMouseOver ? 'hide' : ''}`}>
-                Sechan
-              </div>
-            </div>
         </div>
         
 
