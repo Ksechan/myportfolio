5c4a846e495128eecd24c2e8096edcc02d6fc2ea
Sechan Kim <rlatpcks77@gmail.com>
2021-10-14 18:45:53 +0900

    contact 페이지 제작

--- src/Contact/contact.js
+++ src/Contact/contact.js
@@ -1,13 +1,44 @@
-import react from 'react'
-import HomeBadge from '../homeBadge/homeBadge'
+import react from 'react';
+import HomeBadge from '../homeBadge/homeBadge';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMobileAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Contact() {
   return (
     <div>
-      <HomeBadge/>
-      Contact
+      <HomeBadge />
+      <div className="contact-container text-color-white">
+        <span className="contact-title">Contact</span>
+        <p className="contact-desc">
+          문의사항이 있으시다면 아래연락처를 통해 연락 부탁드립니다.
+        </p>
+        <ul className="contact-list">
+          <li>
+            <FontAwesomeIcon icon={faMobileAlt} className="contact-icon" />
+            <br />
+            <span className="contact-info">+82 10-3356-9426</span>
+          </li>
+
+          <a href="mailto:rlatpcks77@gmail.com" target="_blank">
+            <li>
+              <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+              <br />
+              <span className="contact-info">rlatpcks77@gmail.com</span>
+            </li>
+          </a>
+
+          <a href="https://github.com/rlatpks77" target="_blank">
+            <li>
+              <FontAwesomeIcon icon={faGithub} className="contact-icon" />
+              <br />
+              <span className="contact-info">https://github.com/rlatpks77</span>
+            </li>
+          </a>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Contact;
--- src/LandingPage/landingPage.js
+++ src/LandingPage/landingPage.js
@@ -1,7 +1,5 @@
-import React, { useState } from 'react'
-import {
-  Link
-} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function LandingPage() {
   const [aboutMouseOver, setAboutMouseOver] = useState(false);
@@ -9,68 +7,82 @@ function LandingPage() {
   const [contactMouseOver, setContactMouseOver] = useState(false);
 
   return (
-      
-      <header >
-        
-        <div className="sechan-img">
-        </div>
-  
+    <div className="height-container">
+      <div className="sechan-img"></div>
 
-        <div className="landing-page-container">
-
-          <div className="grow-container">
-            <div className="first-grow"
-              onMouseEnter={() => { setAboutMouseOver(true) }}
-              onMouseLeave={() => { setAboutMouseOver(false) }}>
-                <Link to="/About" className="on-hover40">
-                  <div className={`hello ${aboutMouseOver ? '' : 'hide'}`}>
+      <div className="landing-page-container">
+        <div className="grow-container">
+          <div
+            className="first-grow"
+            onMouseEnter={() => {
+              setAboutMouseOver(true);
+            }}
+            onMouseLeave={() => {
+              setAboutMouseOver(false);
+            }}
+          >
+            <Link to="/About" className="on-hover40">
+              <div
+                className={`text-color-white ${aboutMouseOver ? '' : 'hide'}`}
+              >
                 About
-                  </div>
-                  <div className={`hello ${aboutMouseOver ? 'hide' : ''}`}>
-                Hello.
-                </div>
-                </Link>
-            </div>
-          </div>
-
-
-        
-            <div className="grow-container">
-              <div className="second-grow"
-                onMouseEnter={() => { setWorkMouseOver(true) }}
-                  onMouseLeave={() => { setWorkMouseOver(false) }}>
-                <Link to="/Work" className="on-hover40">
-                  <div className={`text-color-red ${workMouseOver ? '' : 'hide'}`}>
-                 Work
-                  </div>
-                <div className={`text-color-red ${workMouseOver ? 'hide' : ''}`}>
-                  I am
-                </div>
-                </Link>
-
               </div>
-            </div>
-
-
-          <div className="grow-container">
-            <div className="third-grow"
-                onMouseEnter={() => { setContactMouseOver(true) }}
-                  onMouseLeave={() => { setContactMouseOver(false) }}>
-                <Link to="/Contact" className="on-hover40">
-                  <div className={`text-color-red ${contactMouseOver ? '' : 'hide'}`}>
-                Contact
-                  </div>
-                  <div className={`text-color-red ${contactMouseOver ? 'hide' : ''}`}>
-                  Sechan
-                </div>
-                </Link>
-            </div>
+              <div
+                className={`text-color-white ${aboutMouseOver ? 'hide' : ''}`}
+              >
+                Hello.
+              </div>
+            </Link>
           </div>
-          
         </div>
 
-        </header>
+        <div className="grow-container">
+          <div
+            className="second-grow"
+            onMouseEnter={() => {
+              setWorkMouseOver(true);
+            }}
+            onMouseLeave={() => {
+              setWorkMouseOver(false);
+            }}
+          >
+            <Link to="/Work" className="on-hover40">
+              <div className={`text-color-red ${workMouseOver ? '' : 'hide'}`}>
+                Work
+              </div>
+              <div className={`text-color-red ${workMouseOver ? 'hide' : ''}`}>
+                I am
+              </div>
+            </Link>
+          </div>
+        </div>
 
+        <div className="grow-container">
+          <div
+            className="third-grow"
+            onMouseEnter={() => {
+              setContactMouseOver(true);
+            }}
+            onMouseLeave={() => {
+              setContactMouseOver(false);
+            }}
+          >
+            <Link to="/Contact" className="on-hover40">
+              <div
+                className={`text-color-red ${contactMouseOver ? '' : 'hide'}`}
+              >
+                Contact
+              </div>
+              <div
+                className={`text-color-red ${contactMouseOver ? 'hide' : ''}`}
+              >
+                Sechan
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
