ddd20bdf0ce6beebfcad7e16173c3facecd16ad8
Sechan Kim <rlatpcks77@gmail.com>
2021-10-14 22:44:36 +0900

    contact 페이지 마무리작업 및 랜딩페이지 크기수정

--- src/Contact/contact.js
+++ src/Contact/contact.js
@@ -9,32 +9,53 @@ function Contact() {
     <div>
       <HomeBadge />
       <div className="contact-container text-color-white">
-        <span className="contact-title">Contact</span>
-        <p className="contact-desc">
-          문의사항이 있으시다면 아래연락처를 통해 연락 부탁드립니다.
-        </p>
+        <div className="contact-grow-container1">
+          <div className=" contact-grow1">
+            <span className="contact-title">Contact</span>
+            <p className="contact-desc">
+              문의사항이 있으시다면 아래연락처를 통해 연락 부탁드립니다.
+            </p>
+          </div>
+        </div>
+
         <ul className="contact-list">
-          <li>
-            <FontAwesomeIcon icon={faMobileAlt} className="contact-icon" />
-            <br />
-            <span className="contact-info">+82 10-3356-9426</span>
-          </li>
-
-          <a href="mailto:rlatpcks77@gmail.com" target="_blank">
-            <li>
-              <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
-              <br />
-              <span className="contact-info">rlatpcks77@gmail.com</span>
+          <div className="contact-grow-container2">
+            <li className="contact-grow2">
+              <span>
+                <FontAwesomeIcon icon={faMobileAlt} className="contact-icon" />
+                <br />
+                +82 10-3356-9426
+              </span>
             </li>
-          </a>
+          </div>
 
-          <a href="https://github.com/rlatpks77" target="_blank">
-            <li>
-              <FontAwesomeIcon icon={faGithub} className="contact-icon" />
-              <br />
-              <span className="contact-info">https://github.com/rlatpks77</span>
-            </li>
-          </a>
+          <div className="contact-grow-container2">
+            <a
+              href="mailto:rlatpcks77@gmail.com"
+              target="_blank"
+              className="contact-grow2"
+            >
+              <li>
+                <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+                <br />
+                <span>rlatpcks77@gmail.com</span>
+              </li>
+            </a>
+          </div>
+
+          <div className="contact-grow-container2">
+            <a
+              href="https://github.com/rlatpks77"
+              target="_blank"
+              className="contact-grow2"
+            >
+              <li>
+                <FontAwesomeIcon icon={faGithub} className="contact-icon" />
+                <br />
+                <span>https://github.com/rlatpks77</span>
+              </li>
+            </a>
+          </div>
         </ul>
       </div>
     </div>
