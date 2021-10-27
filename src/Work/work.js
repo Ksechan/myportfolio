f7c3067ba780dd224f462654c1df5305ce4baa0f
Sechan Kim <rlatpcks77@gmail.com>
2021-10-27 17:41:24 +0900

    work페이지 작업

--- src/Work/work.js
+++ src/Work/work.js
@@ -1,53 +1,83 @@
+import { useCallback } from 'react';
+import { useEffect, useState, useRef } from 'react/cjs/react.development';
 import HomeBadge from '../homeBadge/homeBadge';
 
-const workArray = [
-  {
-    id: '1',
-    title: 'To Do List',
-    desc: 'This prototype was made with ProtoPie, the interactive prototyping tool for all digital products.',
-    thumbnail: '/thumbnail-images/todolist-thumbnail.gif',
-  },
-  {
-    id: '2',
-    title: 'Prototype shop',
-    desc: 'This prototype was made with ProtoPie, the interactive prototyping tool for all digital products.',
-    thumbnail: '/thumbnail-images/prototypeshop-thumbnail.gif',
-  },
-];
-
 function Work() {
+  const workArray = [
+    {
+      id: '1',
+      title: 'To Do List',
+      subtitle: 'todo check app',
+      desc: 'This prototype was made with ProtoPie, the interactive prototyping tool for all digital products.',
+      thumbnail: '/thumbnail-images/todolist-thumbnail.gif',
+    },
+    {
+      id: '2',
+      title: 'Prototype shop',
+      subtitle: 'prototype design shop',
+      desc: 'This prototype was made with ProtoPie, the interactive prototyping tool for all digital products.',
+      thumbnail: '/thumbnail-images/prototypeshop-thumbnail.gif',
+    },
+  ];
+
+  const clickWorkId = useRef(null);
+
+  const goScroll = (finded) => {
+    console.log(finded.thumbnail);
+    const targetScroll = window.pageYOffset;
+    console.log(targetScroll);
+    window.scrollTo({ top: 80, left: 0, behavior: 'auto' });
+  };
+
+  const scrollToWork = useCallback((id) => {
+    const currentId = id;
+    console.log(currentId);
+    const finded = workArray.find((work) => work.id === currentId);
+
+    if (finded === undefined) {
+      return null;
+    } else {
+      goScroll(finded);
+    }
+  });
+
   return (
     <div className="text-color-white work-margin">
       <HomeBadge />
-      {/* div 태그가 필요없어지면 지워라 */}
-      <div>
-        <ul className="work-list">
-          <li>
-            <h4 className="work-list-title">ToDoList</h4>
-            <span className="work-list-subtitle text-color-red">
-              todo check app
-            </span>
-          </li>
-          <li>
-            <h4 className="work-list-title">Prototype Shop</h4>
-            <span className="work-list-subtitle text-color-red">
-              prototype design shop
-            </span>
-          </li>
-        </ul>
-      </div>
-      <div className="work-container text-color-white">
-        {workArray.map((work) => {
-          const { id, title, desc, thumbnail } = work;
-          return (
-            <div className="works" key={id}>
-              <img src={thumbnail} alt="thumbnail" />
-              <div>{title}</div>
-              <div>{desc}</div>
+      {workArray.map((work) => {
+        const { id, title, subtitle } = work;
+        const click = () => {
+          scrollToWork(id);
+          console.log(id);
+        };
+        return (
+          <div>
+            <ul className="work-list">
+              <li key={id} onClick={click}>
+                <h4 className="work-list-title">{title}</h4>
+                <span className="work-list-subtitle text-color-red">
+                  {subtitle}
+                </span>
+              </li>
+            </ul>
+          </div>
+        );
+      })}
+
+      {workArray.map((work) => {
+        const { id, title, desc, thumbnail } = work;
+        return (
+          <div>
+            <div className="work-container text-color-white">
+              <div className="works" key={id}>
+                <img src={thumbnail} alt="thumbnail" ref={clickWorkId} />
+                <div>{title}</div>
+                <div>{desc}</div>
+              </div>
             </div>
-          );
-        })}
-      </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
